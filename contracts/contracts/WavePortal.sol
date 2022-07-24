3f86ed87a759236bfa731fc56d1d59c0309be8b2
Yinka Adedire <yinkakun@gmail.com>
2022-07-24 22:47:55 +0100

    fix contract bug

--- contracts/contracts/WavePortal.sol
+++ contracts/contracts/WavePortal.sol
@@ -5,7 +5,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    uint256 private seed;
+    uint256 private randomNumber;
 
     event NewWave(address indexed sender, uint256 timestamp, string message);
 
@@ -20,13 +20,11 @@ contract WavePortal {
     mapping(address => uint256) public lastWavedAt;
 
     constructor() payable {
-        console.log("WavePortal Smart Contract");
-        seed = (block.number + block.difficulty) % 100;
-        console.log("Random seed generated: %d", seed);
+        randomNumber = (block.number + block.difficulty) % 100;
     }
 
     function wave(string memory message) public {
-        if ((lastWavedAt[msg.sender] + 15 minutes) < block.timestamp) {
+        if ((lastWavedAt[msg.sender] + 15 minutes) > block.timestamp) {
             revert("Wait at least 15 minutes between waves");
         }
 
@@ -34,17 +32,17 @@ contract WavePortal {
 
         totalWaves = totalWaves + 1;
 
-        seed = (block.number + block.difficulty + seed) % 100;
-        console.log("updated seed %d", seed);
+        randomNumber = (block.number + block.difficulty + randomNumber) % 100;
+        console.log("updated seed %d", randomNumber);
 
         waves.push(Wave(msg.sender, message, block.timestamp));
 
-        if (seed <= 50) {
+        if (randomNumber <= 50) {
             console.log("Waver %s won", msg.sender);
             uint256 prize = 0.001 ether;
 
             if (prize > address(this).balance) {
-                revert("Prize must be less than contract balance");
+                revert("Contract funds is insufficient to grant prize");
             }
 
             (bool sent, ) = (msg.sender).call{value: prize}("");
