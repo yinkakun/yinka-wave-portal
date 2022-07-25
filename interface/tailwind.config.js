5b5fdf2b238b519663e7d6a0d0570ecea7a65c2f
Yinka Adedire <yinkakun@gmail.com>
2022-07-25 12:14:34 +0100

    animate background gradient

--- interface/tailwind.config.js
+++ interface/tailwind.config.js
@@ -8,9 +8,14 @@ module.exports = {
           "0%, 100%": { transform: "rotate(-20deg)" },
           "50%": { transform: "rotate(20deg)" },
         },
+        "bg-position": {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
+        "bg-position": "bg-position 15s ease infinite",
       },
     },
   },
